2c44b88cca679ee6f15220305113f55e82b29d42
KillerDiaz01 <diaz.10fredy@hotmail.com>
2022-02-26 16:26:56 -0600

    Primera actividad en clase y fibonacci

---
+++ TareaClase/BasedeHechosFamilia.pl
@@ -0,0 +1,11 @@
+nombrepadre(luis).
+nombrepadre(ramon).
+nombrepadre(jose).
+
+nombrehijo(carlos).
+nombrehijo(antonio).
+nombrehijo(ricardo).
+
+padre_hijo(luis,carlos).
+padre_hijo(ramon,antonio).
+padre_hijo(jose,ricardo).
